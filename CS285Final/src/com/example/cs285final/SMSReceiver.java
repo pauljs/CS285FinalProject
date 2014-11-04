07c074071d3ad052c4a958d02fe168e94b9fded7
Jbad26 <badash.l.jesse@gmail.com>
2014-11-04 14:58:08 -0600

    initial broadcast recever for sanity purposes

---
+++ CS285Final/src/com/example/cs285final/SMSReceiver.java
@@ -0,0 +1,25 @@
+package com.example.cs285final;
+
+
+import android.content.*;
+import android.os.Bundle;
+import android.telephony.*;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SMSReceiver extends BroadcastReceiver {
+
+ private static final String TAG = "Message recieved";
+
+ @Override
+ public void onReceive(Context context, Intent intent) {    
+     Bundle bundle = intent.getExtras();
+     Object[] pdus = (Object[]) bundle.get("pdus");
+     SmsMessage messages =SmsMessage.createFromPdu((byte[]) pdus[0]); 
+     Toast.makeText(context, "SMS Received : "+messages.getOriginatingAddress(),
+    		 Toast.LENGTH_LONG).show();
+     Log.i(TAG,  messages.getMessageBody());
+ //    Toast.makeText(context, "SMS Received : "+messages.getMessageBody(),
+ //    Toast.LENGTH_LONG).show();
+ }
+}
