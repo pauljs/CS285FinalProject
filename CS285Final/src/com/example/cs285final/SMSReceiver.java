1a0128e64f1c990ec78c165b10379060a84e7759
Erin Hall <efh365@yahoo.com>
2014-11-04 15:42:23 -0600

    Toasts received phone number

--- CS285Final/src/com/example/cs285final/SMSReceiver.java
+++ CS285Final/src/com/example/cs285final/SMSReceiver.java
@@ -1,6 +1,5 @@
 package com.example.cs285final;
 
-
 import android.content.*;
 import android.os.Bundle;
 import android.telephony.*;
@@ -9,17 +8,18 @@ import android.widget.Toast;
 
 public class SMSReceiver extends BroadcastReceiver {
 
- private static final String TAG = "Message recieved";
+	private static final String TAG = "Message recieved";
 
- @Override
- public void onReceive(Context context, Intent intent) {    
-     Bundle bundle = intent.getExtras();
-     Object[] pdus = (Object[]) bundle.get("pdus");
-     SmsMessage messages =SmsMessage.createFromPdu((byte[]) pdus[0]); 
-     Toast.makeText(context, "SMS Received : "+messages.getOriginatingAddress(),
-    		 Toast.LENGTH_LONG).show();
-     Log.i(TAG,  messages.getMessageBody());
- //    Toast.makeText(context, "SMS Received : "+messages.getMessageBody(),
- //    Toast.LENGTH_LONG).show();
- }
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Bundle bundle = intent.getExtras();
+		Object[] pdus = (Object[]) bundle.get("pdus");
+		SmsMessage messages = SmsMessage.createFromPdu((byte[]) pdus[0]);
+		Toast.makeText(context,
+				"SMS Received : " + messages.getOriginatingAddress(),
+				Toast.LENGTH_LONG).show();
+//		Log.i(TAG, messages.getMessageBody());
+		// Toast.makeText(context, "SMS Received : "+messages.getMessageBody(),
+		// Toast.LENGTH_LONG).show();
+	}
 }
