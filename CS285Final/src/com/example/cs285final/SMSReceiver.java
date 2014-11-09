e3afe285e920608e1c59888f9e357afa0984eca1
Jbad26 <badash.l.jesse@gmail.com>
2014-11-09 15:59:19 -0600

    Fixed tabs

--- CS285Final/src/com/example/cs285final/SMSReceiver.java
+++ CS285Final/src/com/example/cs285final/SMSReceiver.java
@@ -37,47 +37,47 @@ public class SMSReceiver extends BroadcastReceiver {
 		}
 	}
 
- /***
-  * Create a Map from the shared preferences location not sure how to do this
-  * @return return a map will all of the registered phone numbers and keys
-  */
- private Map<String, String> generateMap() {
-	 
-	 // TODO : THIS IS A PROBLEM
-	 final SharedPreferences sp = null;
-//	 final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
- 	 String toParse = "";
- 	if(sp.contains("storage")) {
- 		 toParse = sp.getString("storage", "");
- 	}
- 	String[] contacts = toParse.split(";");
- 	Map<String, String> result = new ConcurrentHashMap<String, String>();
- 	for(String contact : contacts){
- 		String[] val = contact.split(",");
- 		result.put(val[0],val[1]);
- 	}
- 	return result;	
-}
+	/***
+	 * Create a Map from the shared preferences location not sure how to do this
+	 * @return return a map will all of the registered phone numbers and keys
+	 */
+	private Map<String, String> generateMap() {
 
-/**
-  * Resends the intent that message was received so that the default
-  * application can catch it and be have like it is natural
-  * @param message : message to be sent
-  * @param sender : string of the phone number that initially sent sent it
-  */
- private void sendInternal(String message, String sender) {
-	// TODO Auto-generated method stub
-	
-}
+		// TODO : THIS IS A PROBLEM
+		final SharedPreferences sp = null;
+		//	 final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+		String toParse = "";
+		if(sp.contains("storage")) {
+			toParse = sp.getString("storage", "");
+		}
+		String[] contacts = toParse.split(";");
+		Map<String, String> result = new ConcurrentHashMap<String, String>();
+		for(String contact : contacts){
+			String[] val = contact.split(",");
+			result.put(val[0],val[1]);
+		}
+		return result;	
+	}
 
-/**
-  * looks up the key associated with the sender and decrypts the message
-  * 
-  * @param message : encrypted text message
-  * @param sender : string of the senders phone number
-  */
- private String decryt(String message, String sender, String key) {
-	// TODO Auto-generated method stub
-	 return null;
- }
+	/**
+	 * Resends the intent that message was received so that the default
+	 * application can catch it and be have like it is natural
+	 * @param message : message to be sent
+	 * @param sender : string of the phone number that initially sent sent it
+	 */
+	private void sendInternal(String message, String sender) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * looks up the key associated with the sender and decrypts the message
+	 * 
+	 * @param message : encrypted text message
+	 * @param sender : string of the senders phone number
+	 */
+	private String decryt(String message, String sender, String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
