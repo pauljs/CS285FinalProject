efc605f87a9b63c403d3cfdfc12113f37cdcb310
Erin Hall <efh365@yahoo.com>
2014-11-09 22:50:21 -0600

    Added basic GUI for texting part of app

--- CS285Final/src/com/example/cs285final/MainActivity.java
+++ CS285Final/src/com/example/cs285final/MainActivity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
@@ -21,125 +22,139 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
-    ListView list;
-    LinearLayout ll;
-    Button loadBtn;
-    ArrayAdapter<String> adapter;
+	ListView list;
+	LinearLayout ll;
+	Button loadBtn;
+	ArrayAdapter<String> adapter;
+	final String PARENT = "com.example.cs285final";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ll = (LinearLayout) findViewById(R.id.LinearLayout1);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ll = (LinearLayout) findViewById(R.id.LinearLayout1);
-
-        list = (ListView) findViewById(R.id.listView1);
-        list.setOnItemClickListener(new OnItemClickListener() {
+		list = (ListView) findViewById(R.id.listView1);
+		list.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(final AdapterView<?> parent, final View view,
-					final int position, final long id) {
+			public void onItemClick(final AdapterView<?> parent,
+					final View view, final int position, final long id) {
 				// TODO Auto-generated method stub
 				final String info = adapter.getItem(position);
-//				Intent intent = new Intent(getApplicationContext(), MessagingActivity.class).putExtra("contact_info", "info");
-//				startActivity(intent);
+				// Intent intent = new Intent(getApplicationContext(),
+				// MessagingActivity.class).putExtra("contact_info", "info");
+				// startActivity(intent);
 			}
 		});
-        loadBtn = (Button) findViewById(R.id.button1);
-        loadBtn.setOnClickListener(new OnClickListener() {
+		loadBtn = (Button) findViewById(R.id.button1);
+		loadBtn.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                final LoadContactsAyscn lca = new LoadContactsAyscn();
-                lca.execute();
-            }
-        });
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				final LoadContactsAyscn lca = new LoadContactsAyscn();
+				lca.execute();
+			}
+		});
+		
+		Button textingView = (Button) findViewById(R.id.button2);
 
-    }
-    
-    //STORAGE IS ONE STRING SEPARATING CONTACTS BY SEMICOLONS AND 
-    // INFORMATION WITHIN THE CONTACT BY COMMAS
-    public String getStorage() {
-    	final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-    	//storage exists so get it
-    	if(sp.contains("storage")) {
-    		return sp.getString("storage", "");
-    	} else {//storage does not exist (AKA first time opening application)
-    		Editor edit = sp.edit();
-    		edit.putString("storage", "");
-    		edit.commit();
-    		return "";
-    	}
 	}
-    
-    //FORMAT: storage is really one string
-    // contacts are separated by semicolons
-    // within a contact, the phoneNumber is first then a comma to separate it from the key
-    public void addToStorage(final String phoneNumber, final String key) {
-    	String storage = getStorage();
-    	storage = storage + ";" + phoneNumber + "," + key;
-    	final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-    	Editor edit = sp.edit();
-    	edit.putString("storage", storage);
-    	while(!edit.commit()) {};
-    }
+	
+	// Starts the new activity for the texting part of the app
+	public void onTextingView(View view) {
+		Intent intent = new Intent(this, TextingView.class);
+		intent.putExtra(PARENT, "test");
+		startActivity(intent);
+	}
 
-    class LoadContactsAyscn extends AsyncTask<Void, Void, ArrayList<String>> {
-        ProgressDialog pd;
+	// STORAGE IS ONE STRING SEPARATING CONTACTS BY SEMICOLONS AND
+	// INFORMATION WITHIN THE CONTACT BY COMMAS
+	public String getStorage() {
+		final SharedPreferences sp = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		// storage exists so get it
+		if (sp.contains("storage")) {
+			return sp.getString("storage", "");
+		} else {// storage does not exist (AKA first time opening application)
+			Editor edit = sp.edit();
+			edit.putString("storage", "");
+			edit.commit();
+			return "";
+		}
+	}
 
-        @Override
-        protected void onPreExecute() {
-            // TODO Auto-generated method stub
-            super.onPreExecute();
+	// FORMAT: storage is really one string
+	// contacts are separated by semicolons
+	// within a contact, the phoneNumber is first then a comma to separate it
+	// from the key
+	public void addToStorage(final String phoneNumber, final String key) {
+		String storage = getStorage();
+		storage = storage + ";" + phoneNumber + "," + key;
+		final SharedPreferences sp = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		Editor edit = sp.edit();
+		edit.putString("storage", storage);
+		while (!edit.commit()) {
+		}
+		;
+	}
 
-            pd = ProgressDialog.show(MainActivity.this, "Loading Contacts",
-                    "Please Wait");
-        }
+	class LoadContactsAyscn extends AsyncTask<Void, Void, ArrayList<String>> {
+		ProgressDialog pd;
 
-        @Override
-        protected ArrayList<String> doInBackground(Void... params) {
-            // TODO Auto-generated method stub
-            final ArrayList<String> contacts = new ArrayList<String>();
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
 
-            final Cursor c = getContentResolver().query(
-                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                    null, null, null);
-            while (c.moveToNext()) {
+			pd = ProgressDialog.show(MainActivity.this, "Loading Contacts",
+					"Please Wait");
+		}
 
-                final String contactName = c
-                        .getString(c
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                final String phNumber = c
-                        .getString(c
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+		@Override
+		protected ArrayList<String> doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			final ArrayList<String> contacts = new ArrayList<String>();
 
-                contacts.add(contactName + ":" + phNumber);
+			final Cursor c = getContentResolver().query(
+					ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+					null, null, null);
+			while (c.moveToNext()) {
 
-            }
-            c.close();
+				final String contactName = c
+						.getString(c
+								.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+				final String phNumber = c
+						.getString(c
+								.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-            return contacts;
-        }
+				contacts.add(contactName + ":" + phNumber);
 
-        @Override
-        protected void onPostExecute(ArrayList<String> contacts) {
-            // TODO Auto-generated method stub
-            super.onPostExecute(contacts);
+			}
+			c.close();
 
-            pd.cancel();
+			return contacts;
+		}
 
-            ll.removeView(loadBtn);
+		@Override
+		protected void onPostExecute(ArrayList<String> contacts) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(contacts);
 
-            adapter = new ArrayAdapter<String>(
-                    getApplicationContext(), R.layout.text, contacts);
+			pd.cancel();
 
-            list.setAdapter(adapter);
+			ll.removeView(loadBtn);
 
-        }
+			adapter = new ArrayAdapter<String>(getApplicationContext(),
+					R.layout.text, contacts);
 
-    }
-    
-    
-    
+			list.setAdapter(adapter);
+
+		}
+
+	}
+
 }
- 
